735f568603b4456df0c7a5c988268551cb84b81c
Nishchitha <nishushetty151997@gmail.com>
2020-02-13 14:29:19 +0530

    new

---
+++ TestingDemo.Tests/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace TestingDemo.Tests.Controllers
+{
+    internal class EmployeeController
+    {
+        public EmployeeController()
+        {
+        }
+
+        internal ViewResult Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestingDemo.Tests/Controllers/EmployeeControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingDemo.Tests.Controllers
+{
+    class EmployeeControllerTest
+    {
+        public void Employees()
+        {
+            // Arrange
+            EmployeeController controller = new EmployeeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
+
